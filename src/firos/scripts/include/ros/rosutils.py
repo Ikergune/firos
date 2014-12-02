ba77425365e5aa631323dd9faccebccaa9f24493
haas85 <ingonza85@gmail.com>
2014-12-02 17:21:54 +0100

    Improved ros integration

--- src/firos/scripts/include/ros/rosutils.py
+++ src/firos/scripts/include/ros/rosutils.py
@@ -4,23 +4,15 @@ def ros2Obj(msgInstance):
         attr = getattr(msgInstance, key)
         if hasattr(attr, '__slots__'):
             obj[key] = ros2Obj(attr)
-        elif key is "data":
-            return attr
         else:
             obj[key] = attr
     return obj
 
-
 def obj2Ros(obj, msgInstance):
     if hasattr(msgInstance, '__slots__'):
-        if len(msgInstance.__slots__) is 1 and msgInstance.__slots__[0] is "data":
-            if type(obj) is dict:
-                raise Exception("Not a primitive")
-            msgInstance.data = obj
-        else:
-            for key in msgInstance.__slots__:
-                if key in obj:
-                    setattr(msgInstance, key, obj2Ros(obj[key], getattr(msgInstance, key)))
+        for key in msgInstance.__slots__:
+            if key in obj:
+                setattr(msgInstance, key, obj2Ros(obj[key], getattr(msgInstance, key)))
     else:
         if type(obj) is dict:
             raise Exception("Not a primitive")
--- src/firos/scripts/test.py
+++ src/firos/scripts/test.py
@@ -6,6 +6,10 @@ import time
 from include.constants import *
 from include.pubsub.pubSubFactory import PublisherFactory
 
+from include.ros.rosutils import *
+
+from std_msgs.msg import String
+
 # Main function.
 if __name__ == '__main__':
     # Initialize the node and name it.
@@ -21,26 +25,30 @@ if __name__ == '__main__':
 
     # print CloudPublisher.createContent("cmd_vel", "NotYet", {"linear": {"x": 5.5, "y": 0.0, "z": 0.0},"angular": {"x": 0.0, "y": 0.0, "z": 0.0}})
     # CloudPublisher.publish("robot2", DEFAULT_CONTEXT_TYPE, [CloudPublisher.createContent("cmd_vel_mux/input/teleop", "NotYet", {"linear": {"x": -0.5, "y": 0.0, "z": 0.0},"angular": {"x": 0.0, "y": 0.0, "z": 0.0}})])
-    CloudPublisher.publish("robot2", DEFAULT_CONTEXT_TYPE, [CloudPublisher.createContent("move_base_simple/goal", "NotYet", {
-        "header": {
-            "seq": 0,
-            "stamp": "now",
-            "frame_id": "map"
-        },
-        "pose": {
-            "position": {
-                "x": 1.0,
-                "y": 1.0,
-                "z": 0.0
-            },
-            "orientation": {
-                "x": 0.2,
-                "y": 0.2,
-                "z": 0.2,
-                "w": 0.4
-            }
-        }
-    })])
+    text = String()
+    text.data = "Hello topic"
+    CloudPublisher.publish("myrobot", DEFAULT_CONTEXT_TYPE, [CloudPublisher.createContent("topic", "NotYet", ros2Obj(text))])
+
+    # CloudPublisher.publish("robot2", DEFAULT_CONTEXT_TYPE, [CloudPublisher.createContent("move_base_simple/goal", "NotYet", {
+    #     "header": {
+    #         "seq": 0,
+    #         "stamp": "now",
+    #         "frame_id": "map"
+    #     },
+    #     "pose": {
+    #         "position": {
+    #             "x": 1.0,
+    #             "y": 1.0,
+    #             "z": 0.0
+    #         },
+    #         "orientation": {
+    #             "x": 0.2,
+    #             "y": 0.2,
+    #             "z": 0.2,
+    #             "w": 0.4
+    #         }
+    #     }
+    # })])
 
     # time.sleep(1)
 
