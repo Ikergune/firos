8f3ed0f96acf114f73ec298c92bc3f03488b9d73
haas85 <ingonza85@gmail.com>
2014-12-23 13:22:27 +0100

    Cleaned code

--- src/firos/scripts/proxy.py
+++ src/firos/scripts/proxy.py
@@ -2,10 +2,7 @@
 
 # Import required Python code.
 import os
-import sys
 import rospy
-import subprocess
-import json
 
 from include.constants import CONTEXTBROKER
 
@@ -14,8 +11,7 @@ PROXY_PATH = "/datos/bamboo_ros_ws/proxy/"
 # Main function.
 if __name__ == '__main__':
     rospy.init_node('proxy', anonymous=True)
-    # subprocess.call("node " + PROXY_PATH)
-    # os.system("node " + PROXY_PATH)
+
     start_command = "node app.js " + CONTEXTBROKER["ADDRESS"] + " " + str(CONTEXTBROKER["PORT"]) + " " + CONTEXTBROKER["PROTOCOL"]
     os.system("cd " + PROXY_PATH + " && npm install && " + start_command)
 
