78ec579a3b4063a6c846338bf75296182dd88287
haas85 <ingonza85@gmail.com>
2015-02-04 14:30:47 +0100

    Changed configuration source

--- src/firos/scripts/include/confManager.py
+++ src/firos/scripts/include/confManager.py
@@ -1,8 +1,18 @@
 import os
 import json
 import traceback
+from include.ros.rosConfigurator import RosConfigurator
 
 def getRobots():
+    try:
+        return RosConfigurator.systemTopics()
+    except Exception as e:
+        traceback.print_exc()
+        print("ERROR: ",e)
+        return []
+
+
+def getRobotsByJson():
     try:
         current_path = os.path.dirname(os.path.abspath(__file__))
         json_path = current_path.replace("scripts/include", "config/robots.json")
