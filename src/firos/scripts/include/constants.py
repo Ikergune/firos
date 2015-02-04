3a55f44d9927123f9b7a194e26b39a1fc12d53e8
haas85 <ingonza85@gmail.com>
2015-02-04 14:34:12 +0100

    Improved configuration management

--- src/firos/scripts/include/constants.py
+++ src/firos/scripts/include/constants.py
@@ -1,23 +1,44 @@
-SERVER = {
-    "ADDRESS" : "0.0.0.0",
-    "PORT"    : 10100
+environment = "local"
+
+CONFIGURATIONS = {
+    "local": {
+        "SERVER": {
+            "ADDRESS" : "0.0.0.0",
+            "PORT"    : 10100
+        },
+        "CONTEXTBROKER": {
+            "ADDRESS": "192.168.4.70",
+            "PORT"    : 1026,
+            "PROTOCOL": "NGSI10"
+        }
+    },
+    "development": {
+        "SERVER": {
+            "ADDRESS" : "0.0.0.0",
+            "PORT"    : 10100
+        },
+        "CONTEXTBROKER": {
+            "ADDRESS" : "130.206.127.115",
+            "PORT"    : 1026,
+            "PROTOCOL": "NGSI10"
+        }
+    },
+    "production": {
+        "SERVER": {
+            "ADDRESS" : "0.0.0.0",
+            "PORT"    : 10100
+        },
+        "CONTEXTBROKER": {
+            "ADDRESS" : "130.206.127.115",
+            "PORT"    : 1026,
+            "PROTOCOL": "NGSI10"
+        }
+    }
 }
 
-# CONTEXT BROKER CONFIG
-# NEW_ADDRESS : "130.206.127.115"
-# OLD_ADDRESS : "130.206.83.196"
+SERVER = CONFIGURATIONS[environment]["SERVER"]
+CONTEXTBROKER = CONFIGURATIONS[environment]["CONTEXTBROKER"]
 
-CONTEXTBROKER = {
-    "ADDRESS" : "130.206.127.115",
-    "PORT"    : 1026,
-    "PROTOCOL": "NGSI10"
-}
-
-# CONTEXTBROKER = {
-#     "ADDRESS" : "127.0.0.1",
-#     "PORT"    : 3000,
-#     "PROTOCOL": "NGSI10"
-# }
 # THROTTLING = "PT1S"
 THROTTLING = "PT0S"
 SUBSCRIPTION_LENGTH = "P1D"
