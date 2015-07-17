81bd540911405d19a2fea7b94133d2f62c962320
haas85 <ingonza85@gmail.com>
2015-07-17 13:16:42 +0200

    Started implementation of kiaraPublisher

--- scripts/include/pubsub/kiara/kiaraPublisher.py
+++ scripts/include/pubsub/kiara/kiaraPublisher.py
@@ -13,8 +13,15 @@
 # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
 # FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+import json
+import time
 
 from include.pubsub.iPubSub import Ipublisher
+from include.pubsub.kiara.kiaraGateway import KiaraGateway
+
+kiara = KiaraGateway()
+PUBLISH_FREQUENCY = 250
+posted_history = {}
 
 
 class kiaraPublisher(Ipublisher):
@@ -24,22 +31,62 @@ class kiaraPublisher(Ipublisher):
         # \param topic name
         # \param topic type
         # \param topic value
-        raise Exception('Method not implemented')
+        data["firosstamp"] = time.time()
+        return {
+            "name": topic,
+            "type": datatype,
+            "value": json.dumps(data).replace('"', SEPARATOR_CHAR)
+        }
 
     def publish(contex_id, datatype, attributes=[]):
-        ## \brief Publish data of an entity
-        # \param entity name
-        # \param entity type
-        # \param entity attributes
-        raise Exception('Method not implemented')
+        ## \brief Publish data of a robot
+        # \param robot name
+        # \param robot type
+        # \param robot attributes
+        _publish(context_id, datatype, attributes)
 
     def publishMap(self, context_id, attributes=[]):
-        ## \brief Publish data of an entity in context broker
+        ## \brief Publish data of a robot
         # \param map topic name
         # \param map connections
-        raise Exception('Method not implemented')
+        _publish(context_id, "MAP", attributes, False)
 
     def publishMsg(attributes=[]):
         ## \brief Publish message structures
-        # \param entity attributes
-        raise Exception('Method not implemented')
+        # \param robot attributes
+        _publish("rosmsg", "ROSDEFINITION", attributes, False)
+
+
+def _publish(context_id, datatype, attributes, sendCommand=True):
+    ## \brief Publish data of an robot
+    # \param robot name
+    # \param robot type
+    # \param robot attributes
+    # \param context broker to send to
+    if context_id not in posted_history:
+        posted_history[context_id] = {}
+    commands = []
+    attr2Send = []
+    current = time.time() * 1000
+    for attribute in attributes:
+        if attribute["name"] not in posted_history[context_id]:
+            posted_history[context_id][attribute["name"]] = 0
+        if (current - posted_history[context_id][attribute["name"]]) > PUBLISH_FREQUENCY:
+            commands.append(attribute["name"])
+            attr2Send.append(attribute)
+            posted_history[context_id][attribute["name"]] = current
+
+    if len(commands) > 0:
+        if(sendCommand):
+            attr2Send.insert(0, {
+                "name": "COMMAND",
+                "type": "COMMAND",
+                "value": commands
+            })
+        data = {
+            "id": context_id,
+            "type": datatype,
+            "attributes": attr2Send
+        }
+
+        kiara.sendData(json.dumps(data))
