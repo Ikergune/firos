75f008eafee79af076e19c220b60bbf6dae3aa4f
haas85 <ingonza85@gmail.com>
2015-07-17 13:50:45 +0200

    Connection data from arguments

--- conectors/kiara/src/com/firos/kiara/Main.java
+++ conectors/kiara/src/com/firos/kiara/Main.java
@@ -2,9 +2,32 @@ package com.firos.kiara;
 
 import com.firos.kiara.connector.Connector;
 
+import py4j.GatewayServer;
+
 public class Main {
+	
+	public static String KIARA_IP = "239.255.0.1";
+	public static int KIARA_PORT = 7400;
+	
+	private static void loadArgs(String[] args){
+		for (int i = 0; i < args.length; i++) {
+			if(args[i] == "-a"){
+				i++;
+				KIARA_IP = args[i];
+			}
+			if(args[i] == "-p"){
+				i++;
+				KIARA_PORT = Integer.parseInt(args[i]);
+			}
+		}		
+	}
 
 	public static void main(String[] args) {
+		loadArgs(args);
+	    Main app = new Main();
+	    // app is now the gateway.entry_point
+	    GatewayServer server = new GatewayServer(app);
+	    server.start();
 		// TODO Auto-generated method stub
 		Connector connector = Connector.getInstance();
 	}
--- conectors/kiara/src/com/firos/kiara/connector/Connector.java
+++ conectors/kiara/src/com/firos/kiara/connector/Connector.java
@@ -1,18 +1,84 @@
 package com.firos.kiara.connector;
 
+import com.firos.kiara.Main;
 import com.firos.kiara.interfaces.Firos;
 
 import py4j.GatewayServer;
+
+//Kiara Depndencies
+import org.fiware.kiara.ps.rtps.RTPSDomain;
+
+import org.fiware.kiara.ps.rtps.common.Locator;
+import org.fiware.kiara.ps.rtps.common.ReliabilityKind;
+import org.fiware.kiara.ps.rtps.common.EncapsulationKind;
+
+import org.fiware.kiara.ps.rtps.history.CacheChange;
+import org.fiware.kiara.ps.rtps.history.WriterHistoryCache;
+
+import org.fiware.kiara.ps.rtps.attributes.WriterAttributes;
+import org.fiware.kiara.ps.rtps.attributes.RemoteReaderAttributes;
+import org.fiware.kiara.ps.rtps.attributes.HistoryCacheAttributes;
+import org.fiware.kiara.ps.rtps.attributes.RTPSParticipantAttributes;
+
+import org.fiware.kiara.ps.rtps.messages.common.types.ChangeKind;
+import org.fiware.kiara.ps.rtps.messages.elements.InstanceHandle;
+//import org.fiware.kiara.ps.rtps.messages.elements.SerializedPayload;
+
+import org.fiware.kiara.ps.rtps.writer.RTPSWriter;
+import org.fiware.kiara.ps.rtps.participant.RTPSParticipant;
+
+import org.fiware.kiara.serialization.impl.Serializable;
 
 public class Connector {
 	
 	private static Connector connector = new Connector();
 	private GatewayServer server; 
+	
 	private Firos firos;
+	private RTPSWriter writer = null;
+    private WriterHistoryCache w_history  = null;
+    private RTPSParticipantAttributes pparam = new RTPSParticipantAttributes();
 	
 	public Connector(){
+		//	GateWay methods
 		server = new GatewayServer(this);
 	    server.start();
+	    
+	    // Kiara Methods
+        pparam.builtinAtt.useSimplePDP = false;
+        pparam.builtinAtt.useWriterLP = false;
+
+        RTPSParticipant participant = RTPSDomain.createParticipant(pparam, null);
+
+        if(participant == null)
+        {
+            System.out.println("ERROR creating participant");
+            return;
+        }
+
+        HistoryCacheAttributes hatt = new HistoryCacheAttributes();
+        hatt.payloadMaxSize = 500;
+
+        w_history = new WriterHistoryCache(hatt);
+
+        WriterAttributes watt = new WriterAttributes();
+        watt.endpointAtt.reliabilityKind = ReliabilityKind.BEST_EFFORT;
+
+        writer = RTPSDomain.createRTPSWriter(participant, watt, w_history, null);
+
+        if(writer == null)
+        {
+            System.out.println("ERROR creating writer");
+            return;
+        }
+
+        RemoteReaderAttributes ratt = new RemoteReaderAttributes();
+        Locator loc = new Locator();
+        loc.setIPv4Address(Main.KIARA_IP);
+        loc.setPort(Main.KIARA_PORT);
+
+        ratt.endpoint.multicastLocatorList.pushBack(loc);
+        writer.matchedReaderAdd(ratt);
 	}
 	
 	public static Connector getInstance() {
@@ -28,9 +94,17 @@ public class Connector {
 		this.firos.onData(data);
 	}
 	
-	public void sendMessage(String message){
+	public void sendMessage(String data){
 		System.out.println("SEND");
-		System.out.println(message);
+		System.out.println(data);
+		CacheChange change = writer.newChange(ChangeKind.ALIVE, new InstanceHandle());
+        Message message = new Message();
+        message.setStr(data);
+
+        change.getSerializedPayload().setEncapsulationKind(EncapsulationKind.CDR_LE);
+        change.getSerializedPayload().setData((Serializable)message);
+        change.getSerializedPayload().setLength((short)(4 + 255));
+        w_history.addChange(change);
 	}
 	
 	public void stop(){
